94255610f248178d37ce9db5dd810489ea041c2e
Pimcraft <166647935+Pimcraft@users.noreply.github.com>
2025-03-25 14:25:54 +0100

    fix

--- main.js
+++ main.js
@@ -20,9 +20,10 @@ document.addEventListener('DOMContentLoaded', () => {
 /** Tasowanie tablicy (in-place) */
 function shuffleArray(arr) {
     for (let i = arr.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
+        const j = Math.floor(Math.random() * (i + 1));
         [arr[i], arr[j]] = [arr[j], arr[i]];
     }
+    return arr; // dodajemy return!
 }
 /**
  * startQuiz():
